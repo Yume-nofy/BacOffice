61745b22975ca672bb3baa0c8d1a4a5feb973861
Loic Dylan Rakotoarivony <loicrakotoarivony07@gmail.com>
2026-03-25 16:12:33 +0300

    Update startup.bat

--- Test/startup.bat
+++ Test/startup.bat
@@ -3,7 +3,7 @@ echo ================================
 echo   Switching JAVA_HOME to JDK 17
 echo ================================
 
-set "JAVA_HOME=C:\Program Files\jdk-17.0.13.11-hotspot"
+set "JAVA_HOME=C:\Program Files\Java\jdk-17"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 
 echo JAVA_HOME is now: %JAVA_HOME%
@@ -13,7 +13,7 @@ echo ================================
 echo   Starting Apache Tomcat
 echo ================================
 
-cd /d "C:\apache-tomcat-10.1.34\bin"
+cd /d "C:\Program Files\apache-tomcat-10.1.28\bin"
 
 startup.bat
 
